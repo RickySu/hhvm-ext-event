0006eaf30e9cafb1a21416751da39acf9ed71bb6
Ricky Su <ricky@ez2.us>
2014-05-28 18:35:12 +0800

    add lib detection

--- merge_hni_php.sh
+++ merge_hni_php.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 printf "<?hh\n" > ext_event.php
-tail -q -n +2 src/*php src/types/*php src/exceptions/*php >> ext_event.php
+tail -q -n +2 src/*.php src/types/*.php src/exceptions/*.php >> ext_event.php
