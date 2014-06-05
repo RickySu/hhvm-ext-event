bba7b619ef2d559ba2d2f27df707de14e73cc328
Ricky Su <ricky@ez2.us>
2014-06-05 11:36:59 +0800

    reorder hni class methods

--- src/ext_event_http_request.php
+++ src/ext_event_http_request.php
@@ -12,14 +12,13 @@
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    const int INPUT_HEADER = 1 ;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    const int OUTPUT_HEADER = 2 ;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    private ?resource $__internal_resource = null;
-<*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function __construct(?callable $callback, ?mixed $data = null): void;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function addHeader(string $key, string $value, int $type): bool;
-<*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function free(): void;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function cancel(): void;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function clearHeaders(): void;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function closeConnection(): void;
-<*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function sendReply(int $code, string $reason, ?EventBuffer $buf = null): void;
+<*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function __construct(?callable $callback, ?mixed $data = null): void;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function findHeader(string $key, int $type): string;
+<*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function free(): void;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function getEventBufferEvent(): ?EventBufferEvent;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function getCommand(): int;
 //<*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function getConnection(): ?EventHttpConnection;
@@ -32,7 +31,8 @@
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function getUri(): string;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function removeHeader(string $key, int $type): bool;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function sendError(int $error, ?string $reason = null): void;
-<*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function sendReplyStart(int $code, string $reason): void;
+<*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function sendReply(int $code, string $reason, ?EventBuffer $buf = null): void;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function sendReplyChunk(EventBuffer $buf): void;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function sendReplyEnd(): void;
+<*HAVE_LIBEVENT_HTTP_SUPPORT*>    <<__Native>> function sendReplyStart(int $code, string $reason): void;
 <*HAVE_LIBEVENT_HTTP_SUPPORT*>}
