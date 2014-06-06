4e0049e886ca8d9f64f78254a0ba8c0f50a12ffc
Ricky Su <ricky@ez2.us>
2014-06-06 15:49:09 +0800

    add EventDNSBase::parseResolvConf

--- src/ext_event_dns_base.cpp
+++ src/ext_event_dns_base.cpp
@@ -2,14 +2,79 @@
 
 #ifdef HAVE_LIBEVENT_DNS_SUPPORT
 namespace HPHP {
+
     static void HHVM_METHOD(EventDNSBase, __construct, const Object &base, bool initialize) {
         InternalResourceData *event_base_resource_data = FETCH_RESOURCE(base, InternalResourceData, s_event_base);
         Resource resource = Resource(NEWOBJ(InternalResourceData(evdns_base_new((event_base_t *) event_base_resource_data->getInternalResourceData(), initialize))));
         SET_RESOURCE(this_, resource, s_event_dns_base);
     }
 
+    static bool HHVM_METHOD(EventDNSBase, addNameserverIp, const String &ip) {
+        InternalResourceData *event_dns_base_resource_data = FETCH_RESOURCE(this_, InternalResourceData, s_event_dns_base);
+        return evdns_base_nameserver_ip_add((evdns_base_t *) event_dns_base_resource_data->getInternalResourceData(), ip.c_str()) == 0;
+    }
+
+    static void HHVM_METHOD(EventDNSBase, addSearch, const String &domain) {
+        InternalResourceData *event_dns_base_resource_data = FETCH_RESOURCE(this_, InternalResourceData, s_event_dns_base);
+        evdns_base_search_add((evdns_base_t *) event_dns_base_resource_data->getInternalResourceData(), domain.c_str());
+    }
+
+    static void HHVM_METHOD(EventDNSBase, clearSearch) {
+        InternalResourceData *event_dns_base_resource_data = FETCH_RESOURCE(this_, InternalResourceData, s_event_dns_base);
+        evdns_base_search_clear((evdns_base_t *) event_dns_base_resource_data->getInternalResourceData());
+    }
+
+    static int64_t HHVM_METHOD(EventDNSBase, countNameservers) {
+        InternalResourceData *event_dns_base_resource_data = FETCH_RESOURCE(this_, InternalResourceData, s_event_dns_base);
+        return evdns_base_count_nameservers((evdns_base_t *) event_dns_base_resource_data->getInternalResourceData());
+    }
+
+    static bool HHVM_METHOD(EventDNSBase, loadHosts, const String &hosts) {
+        InternalResourceData *event_dns_base_resource_data = FETCH_RESOURCE(this_, InternalResourceData, s_event_dns_base);
+        return evdns_base_load_hosts((evdns_base_t *) event_dns_base_resource_data->getInternalResourceData(), hosts.c_str()) == 0;
+    }
+
+    static bool HHVM_METHOD(EventDNSBase, parseResolvConf, int64_t flags, const String &filename) {
+        int ret;
+        InternalResourceData *event_dns_base_resource_data = FETCH_RESOURCE(this_, InternalResourceData, s_event_dns_base);
+        if(flags & ~(DNS_OPTION_NAMESERVERS | DNS_OPTION_SEARCH | DNS_OPTION_MISC  | DNS_OPTIONS_ALL)) {
+            raise_warning("Invalid flags");
+            return false;
+        }
+        ret = evdns_base_resolv_conf_parse((evdns_base_t *) event_dns_base_resource_data->getInternalResourceData(), flags, filename.c_str());
+        if(ret){
+            switch(ret){
+                case 1:
+                     raise_warning("Failed to open file");
+                     break;
+                case 2:
+                     raise_warning("Failed to stat file");
+                     break;
+                case 3:
+                     raise_warning("File too large");
+                     break;
+                case 4:
+                     raise_warning("Out of memory");
+                     break;
+                case 5:
+                     raise_warning("Short read from file");
+                     break;
+                case 6:
+                     raise_warning("No nameservers listed in the file");
+                     break;
+            }
+        }
+        return true;
+    }
+
     void eventExtension::_initEventDNSBaseClass() {
         HHVM_ME(EventDNSBase, __construct);
+        HHVM_ME(EventDNSBase, addNameserverIp);
+        HHVM_ME(EventDNSBase, addSearch);
+        HHVM_ME(EventDNSBase, clearSearch);
+        HHVM_ME(EventDNSBase, countNameservers);
+        HHVM_ME(EventDNSBase, loadHosts);
+        HHVM_ME(EventDNSBase, parseResolvConf);
     }
 }
 #endif
--- src/ext_event_dns_base.php
+++ src/ext_event_dns_base.php
@@ -8,4 +8,10 @@
 <*HAVE_LIBEVENT_DNS_SUPPORT*>    const int OPTIONS_ALL = 15 ;
 <*HAVE_LIBEVENT_DNS_SUPPORT*>    private ?resource $__internal_resource = null;
 <*HAVE_LIBEVENT_DNS_SUPPORT*>    <<__Native>> function __construct(EventBase $base, bool $initialize): void;
+<*HAVE_LIBEVENT_DNS_SUPPORT*>    <<__Native>> function addNameserverIp(string $ip): bool;
+<*HAVE_LIBEVENT_DNS_SUPPORT*>    <<__Native>> function addSearch(string $domain): void;
+<*HAVE_LIBEVENT_DNS_SUPPORT*>    <<__Native>> function clearSearch(): void;
+<*HAVE_LIBEVENT_DNS_SUPPORT*>    <<__Native>> function countNameservers(): int;
+<*HAVE_LIBEVENT_DNS_SUPPORT*>    <<__Native>> function loadHosts(string $hosts): bool;
+<*HAVE_LIBEVENT_DNS_SUPPORT*>    <<__Native>> function parseResolvConf(int $flags, string $filename): bool;
 <*HAVE_LIBEVENT_DNS_SUPPORT*>}
